b3be405ae81f06c6bc9207026846d58363fe3de3
Scott Pierce <ddrscott@gmail.com>
2020-04-21 11:05:14 -0500

    cleanup

--- components/game.js
+++ components/game.js
@@ -1,58 +1,10 @@
 import React  from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 import Phaser from 'phaser';
 import Board from '../lib/board';
 import Preload from '../lib/preload';
 import Overlay from '../lib/overlay';
-
-const BONUS = {
-    inc: 100,
-    decay: 0.25
-}
-
-const Underlay = styled.section`
-    position: absolute;
-    top: 0; left: 0; bottom: 0; right: 0;
-    padding: 0.25em;
-    user-select: none;
-    pointer-events: none;
-    height: 800px; /* FIXME we shouldn't need this! */
-`
-
-const Score = styled.span`
-    font-size: 1.5em;
-`;
-
-const Title = styled.h1`
-    color: #B78E49;
-    font-weight: bold;
-    margin: 0;
-    padding: 0 .5em;
-    opacity: 0.75;
-    text-align: left;
-`
-const MapName = styled.div`
-    ----webkit-text-stroke: 1px #B78E49;
-    font-size: 1.5em;
-    font-family: sans-serif;
-    font-weight: 900;
-    padding: 0;
-    margin-left: .25em;
-    margin-top: -2.2em;
-    text-align: left;
-    transform: rotate(-15deg);
-    opacity: 0.75;
-    color: #511;
-`
-
-const MapSup = styled.sup`
-    -webkit-text-stroke: 0;
-    font-size: .5em;
-    margin-bottom: -.5em;
-    display: block;
-`
 
 class Game extends React.Component {
 
